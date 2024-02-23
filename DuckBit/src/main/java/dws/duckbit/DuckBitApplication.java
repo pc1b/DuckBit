835e1fb1e5dad507b923f2a5c9fce543ace93762
plopez-a <112652766+pc1b@users.noreply.github.com>
2024-02-23 11:17:12 +0100

    Create project

---
+++ DuckBit/src/main/java/dws/duckbit/DuckBitApplication.java
@@ -0,0 +1,13 @@
+package dws.duckbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckBitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckBitApplication.class, args);
+	}
+
+}
---
+++ DuckBit/src/test/java/dws/duckbit/DuckBitApplicationTests.java
@@ -0,0 +1,13 @@
+package dws.duckbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckBitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
