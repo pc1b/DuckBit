6ff02643404aca5ad391b14402ab4cb1bebaf964
dtorresss <diegotorresprofesional@gmail.com>
2024-02-26 16:44:43 +0100

    Home page and admin retouched and image upload function more or less done

--- DuckBit/src/main/java/dws/duckbit/WebController.java
+++ DuckBit/src/main/java/dws/duckbit/WebController.java
@@ -3,9 +3,26 @@ package dws.duckbit;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.UrlResource;
+
+import java.net.MalformedURLException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class WebController {
+    private static int i = 0;
+    private String nameFile ="";
+    private static final Path IMAGES_FOLDER = Paths.get("../images");
+    private String imageName;
 
     // INDEX
 
@@ -57,6 +74,35 @@ public class WebController {
     public String shop_admin()
     {
         return "shop_admin";
+    }
+
+    @PostMapping("/upload_image")
+    public String uploadImage(@RequestParam String imageName,
+                              @RequestParam MultipartFile image, Model model) throws IOException {
+        this.imageName = imageName;
+        Files.createDirectories(IMAGES_FOLDER);
+        nameFile = "profile" + i + ".jpg";
+        i++;
+        Path imagePath = IMAGES_FOLDER.resolve(nameFile);
+        image.transferTo(imagePath);
+        model.addAttribute("imageName", imageName);
+        return "image";
+    }
+
+    @GetMapping("/image")
+    public String viewImage(Model model) {
+        model.addAttribute("imageName", imageName);
+        return "view_image";
+    }
+
+    @GetMapping("/download_image")
+    public ResponseEntity<Object> downloadImage(Model model)
+            throws MalformedURLException {
+        Path imagePath = IMAGES_FOLDER.resolve(nameFile);
+        Resource image = new UrlResource(imagePath.toUri());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
+                .body(image);
     }
 
 }
