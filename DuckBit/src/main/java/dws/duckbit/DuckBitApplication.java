b799cb23265b5f7f86c5b4f1305dd10d35f28182
focab0r <93092676+focab0r@users.noreply.github.com>
2024-03-10 20:30:31 +0100

    Fixed style in UserService and DuckBitApplication (Norminette)

--- DuckBit/src/main/java/dws/duckbit/DuckBitApplication.java
+++ DuckBit/src/main/java/dws/duckbit/DuckBitApplication.java
@@ -3,11 +3,12 @@ package dws.duckbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DuckBitApplication {
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class DuckBitApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DuckBitApplication.class, args);
 	}
-
 }
