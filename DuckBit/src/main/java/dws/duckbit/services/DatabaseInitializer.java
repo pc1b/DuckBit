762a2c4c988973d3979364de47b1724b5d5733f7
plopez-a <112652766+pc1b@users.noreply.github.com>
2024-04-10 11:58:14 +0200

    default images in DB

--- DuckBit/src/main/java/dws/duckbit/services/DatabaseInitializer.java
+++ DuckBit/src/main/java/dws/duckbit/services/DatabaseInitializer.java
@@ -1,17 +1,26 @@
 package dws.duckbit.services;
 
+import dws.duckbit.entities.UserD;
 import jakarta.annotation.PostConstruct;
+
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Component
 public class DatabaseInitializer {
 
 	@Autowired
 	private LeakService leakService;
+	private final Path IMAGES_FOLDER = Paths.get("src/main/resources/static/images");
 
 	@Autowired
 	private ComboService comboService;
@@ -32,9 +41,19 @@ public class DatabaseInitializer {
 		id.remove(1);
 		id.add(1L);
 		this.comboService.save(this.comboService.createCombo("Combo2", id, 40, "<p>El segundo mejor <strong>Combo </strong>que vas a ver en tu <strong><em>vida</em></strong>, asi es como te lo <em>digo</em>!!!!</p>"));
+
+
 		this.userService.addUser("admin", "admin@duckbit.org", "admin");
 		this.userService.addUser("paco", "paco@duckbit.org", "paco");
 		this.userService.addUser("juan", "juan@duckbit.org", "juan");
+		for (int i = 1; i<=3; i++){
+			UserD u =  this.userService.findByID((long)i).orElseThrow();
+			Path imagePath = IMAGES_FOLDER.resolve("admin.jpg");
+			Resource image = new UrlResource(imagePath.toUri());
+			u.setImageFile(BlobProxy.generateProxy(image.getInputStream(), image.getFile().length()));
+			this.userService.save(u);
+		}
+
 
 	}
 
